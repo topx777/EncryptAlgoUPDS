e5f0f9202621db23f254e8637893ff07c4fde892
topx777 <topx777@gmail.com>
2019-10-16 12:12:03 -0400

    CESAR

---
+++ assets/js/algos/cesar.js
@@ -0,0 +1,9 @@
+//Funcion para encriptar en cesar
+function encryptCesar(text) {
+	let dictionary = [
+		'A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 
+		'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z', 'a', 'b', 'c'
+	]
+
+
+}
